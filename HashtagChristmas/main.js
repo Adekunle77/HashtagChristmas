28618149904e482c2a5b8b46d115a0c5886c55db
Adekunle77 <g.77@virgin.net>
2015-11-26 11:24:34 +0000

    TweenMax included

--- HashtagChristmas/main.js
+++ HashtagChristmas/main.js
@@ -1,21 +1,76 @@
 $(document).ready(function(){
     
+    $("#contshdow").hide()
+// TweenMax Animation 
+    
+    // creating a varibale to get id 
+    //var cont1 = document.getElementById("cont1");
+    var revesreCout1 = TweenMax.to(document.getElementById("cont1"), 1.2, {scale:0.2, opacity:0, ease:Cubic.easeOut})
+    
+    revesreCout1.reverse(0);
+    
+    
+   $("#cont2-3").click(function(){
+        var x = $("#cont2-3").position();
+        alert("Top position: " + x.top + " Left position: " + x.left);
+    });
+    
+    
+    
+    
+    
 // On click for for block 1    
 
     $("#cont1").click(function(){
+        $("#contshdow").show()
         $("#cont1").hide()
-        $("#cont2").show()
+        $("#cont2-1").fadeIn(0400)
+        $("#cont2-2").fadeIn(0400)
+        $("#cont2-3").fadeIn(0400)
+        $("#cont2-4").fadeIn(0400)
+        
+                TweenMax.to(document.getElementById("contShd1"), 0.6, {ease:Cubic.easeOut, x:-100, y:-100, scale:0.5 })        
+        
+        TweenMax.to(document.getElementById("contShd2"), 0.6, {ease:Cubic.easeOut, x:132, y:-127, scale:0.5 })
+        
+        TweenMax.to(document.getElementById("contShd3"), 0.6, {ease:Cubic.easeOut, x:-123, y:127, scale:0.5 })
+        
+        TweenMax.to(document.getElementById("contShd4"), 0.6, {ease:Cubic.easeOut, x:132, y:127, scale:0.5 }) 
+    
+    
     })
  
-    $("#cont2 div").click(function(){
-        var clickedDiv = $(this)
-        var clickedClass = clickedDiv.attr('class')
-//        console.log(this)
-        clickedDiv.hide()
+// On click for for block 4 and block 16     
+    
+    //block top left
+    $("#cont2-1").click(function() {
+        $(this).remove();
+        $(".cont2-1").show() 
+        $("#contShd1").hide()
+    })
+    //block top right
+    $("#cont2-2").click(function() {
+        $(this).remove();  
+        $(".cont2-2").show()
+        $("#contShd2").hide()
+    }) 
+    
+    //block bottom left
+    $("#cont2-3").click(function() {
+        $(this).remove();  
+        $(".cont2-3").show() 
+        $("#contShd3").hide()
         
-        // display the 4 little bubbles underneath..
-        var selector = "#cont3 div." + clickedClass
-        console.log(selector)
-        $(selector).show()
-    })    
+//        var x = $("#cont2-3").position();
+        alert("Top position: " + x.top + " Left position: " + x.left);
+        
+    }) 
+    
+    //block bottom right
+    $("#cont2-4").click(function() {
+        $(this).remove();  
+        $(".cont2-4").show()
+        $("#contShd4").hide()
+    })   
+
 })
